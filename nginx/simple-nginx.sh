ddc06a05e2698ea5dd22bc9c63c952bf3759668d
dsudo <dsudo0624@gmail.com>
2018-06-12 22:49:11 +0900

    Add ruby-bundle-init

--- nginx/simple-nginx.sh
+++ nginx/simple-nginx.sh
@@ -1,6 +1,9 @@
+export NGINX_VERSION=mainline-alpine
+export PORT=8080
+export PWD=/c/nginx/html
 docker run \
   --name simple-nginx \
   --rm -it \
-  -p 8080:80 \
-  -v /c/nginx/html:/usr/share/nginx/html:ro \
-  nginx:mainline-alpine
+  -p $PORT:80 \
+  -v $PWD:/usr/share/nginx/html:ro \
+  nginx:$NGINX_VERSION
