47ae4d2bcede4c3b87e5cfa0c097d1f87de65a6f
dsudo <dsudo0624@gmail.com>
2018-06-21 23:38:20 +0900

    Add ruby-dev

---
+++ ruby/ruby-irb.sh
@@ -0,0 +1,8 @@
+export RUBY_VERSION=2.5.1-alpine3.7
+export PWD=`pwd | sed -r 's/\/mnt(.*)/\1/'`
+docker run \
+  --name ruby-irb \
+  --rm -it \
+  -v $PWD:/usr/src \
+  -w /usr/src \
+  ruby:$RUBY_VERSION
