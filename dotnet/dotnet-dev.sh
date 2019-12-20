d401debd028fb595b988c491bd8d7225472f894a
dsudo <dsudo0624@gmail.com>
2019-12-20 20:55:45 +0900

    Update dotnet-dev.sh

--- dotnet/dotnet-dev.sh
+++ dotnet/dotnet-dev.sh
@@ -1,9 +1,9 @@
-export DOTNET_VERSION=2.2-sdk
+export DOTNET_VERSION=3.1-bionic
 export PWD=`pwd | sed -r 's/\/mnt(.*)/\1/'`
 docker run \
   --name dotnet-dev \
   --rm -it \
   -w /home/dotnet \
   -v $PWD:/home/dotnet \
-  microsoft/dotnet:$DOTNET_VERSION \
+  mcr.microsoft.com/dotnet/core/sdk:$DOTNET_VERSION \
   bash
